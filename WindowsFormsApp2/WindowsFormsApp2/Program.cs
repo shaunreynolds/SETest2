5e12647b22db58810e4e02001ea3de6665ae4bf5
Roland <401627@edu.cwa.ac.uk>
2017-09-19 09:52:31 +0100

    added comment

--- WindowsFormsApp2/WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp2
         [STAThread]
         static void Main()
         {
+            // hello from roland
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
